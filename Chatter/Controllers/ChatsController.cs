23789b61e4816c79406e537deff3023460434f5f
joderrick1022 <joderrick1022>
2017-11-15 12:52:58 -0500

    finished part 3

--- Chatter/Controllers/ChatsController.cs
+++ Chatter/Controllers/ChatsController.cs
@@ -21,6 +21,15 @@ namespace Chatter.Controllers
             return View(chats.ToList());
         }
 
+        public JsonResult TestJson()
+        {
+            string jsonTest = "{ \"firstName\": \"Bob\",\"lastName\": \"Sauce\", \"children\": [{\"firstName\": \"Barbie\", \"age\": 19 },{\"firstName\": \"Ron\", \"age\": null }] }";
+
+            return Json(jsonTest, JsonRequestBehavior.AllowGet);
+        }
+
+
+
         // GET: Chats/Details/5
         public ActionResult Details(int? id)
         {
